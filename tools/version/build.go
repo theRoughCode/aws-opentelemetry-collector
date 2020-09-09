6e02e991de42cefb3907631908723fa2036aad7e
Ying Wang <wangmyin@amazon.com>
2020-09-09 12:09:37 -0700

    fix naming

--- tools/version/build.go
+++ tools/version/build.go
@@ -28,7 +28,7 @@ var (
 )
 
 func init() {
-	fmt.Printf("AWS OpenTelemetry Collector version: %v\n", Version)
+	fmt.Printf("AWS Observability Collector version: %v\n", Version)
 }
 
 // Info has properties about the build and runtime.
